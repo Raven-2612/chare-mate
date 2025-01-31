7c664a5722fde814c491b6555ae7ad0664a242b6
Raven-2612 <Indianpolicepandiyan@gmail.com>
2025-01-31 14:58:25 +0530

    Update supabase.ts

--- project/src/lib/supabase.ts
+++ project/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://klcmbbsqvbjxdjhtqzyf.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsY21iYnNxdmJqeGRqaHRxenlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzMDcyMDYsImV4cCI6MjA1Mzg4MzIwNn0.VLghcwLEqlbXj-f1g8eUs_qwfcku-TWMMuQhPq3PV80";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
